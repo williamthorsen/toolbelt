6d0781e02e2d50b382b81314382f17bc0fa54fa3
William Thorsen <william@thorsen.dev>
2025-05-15 14:21:15 -0700

    sets|feat: ⛔ Migrate from toolbelt-deno

--- packages/sets/src/3-candidate/index.ts
+++ packages/sets/src/3-candidate/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { setDifference } from './setDifference.ts';
+export { setIntersection } from './setIntersection.ts';
+export { setIsSubset } from './setIsSubset.ts';
+export { setsAreEqual } from './setsAreEqual.ts';
+export { setSymmetricDifference } from './setSymmetricDifference.ts';
