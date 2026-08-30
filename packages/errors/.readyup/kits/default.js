0792c6022fbae6a10c809bd71ddd38199b7f3196
William Thorsen <william@thorsen.dev>
2026-08-30 09:44:29 -0700

    root|deps: Upgrade all deps to latest version

--- packages/errors/.readyup/kits/default.js
+++ packages/errors/.readyup/kits/default.js
@@ -1,6 +1,6 @@
 /** @noformat -- @generated. Do not edit. Compiled by rdy. */
 /* eslint-disable */
-export const __readyupVersion = "0.34.0";
+export const __readyupVersion = "0.35.0";
 
 
 // ../adoption/src/conventions/path-predicates.ts
